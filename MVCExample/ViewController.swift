8006396f0bd12f955e8375171062d6d1de778168
Samuel Goodwin <samuel@roundwallsoftware.com>
2014-11-08 10:25:46 +0100

    Initial Commit

---
+++ MVCExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVCExample
+//
+//  Created by Samuel Goodwin on 11/8/14.
+//  Copyright (c) 2014 Roundwall Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
