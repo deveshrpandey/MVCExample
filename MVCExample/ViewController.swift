5817ce05bd6310dc72ae25141a40caf5459c023e
Samuel Goodwin <samuel@roundwallsoftware.com>
2014-11-08 10:49:03 +0100

    An example for my homie.

--- MVCExample/ViewController.swift
+++ MVCExample/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
+struct Model {
+    let leftSideInput: String
+    let rightSideInput: String
+    
+    func leftSideValue() -> Int {
+        return leftSideInput.toInt()!
+    }
+    
+    func rightSideValue() -> Int {
+        return rightSideInput.toInt()!
+    }
+}
+
+func determineResult(model: Model) -> String {
+    let formatter = NSNumberFormatter()
+    formatter.locale = NSLocale.currentLocale()
+    formatter.numberStyle = .DecimalStyle
+    
+    let leftSideString = formatter.stringFromNumber(model.leftSideValue())!
+    let rightSideString = formatter.stringFromNumber(model.rightSideValue())!
+    
+    if model.leftSideValue() > model.rightSideValue() {
+        return "\(leftSideString) > \(rightSideString)"
+    } else {
+        return "\(leftSideString) < \(rightSideString)"
+    }
+}
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var leftSideField: UITextField!
+    @IBOutlet weak var rightSideField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func calculate(sender: UIButton) {
+        let model = Model(leftSideInput: leftSideField.text, rightSideInput: rightSideField.text)
+        
+        resultLabel.text = determineResult(model)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
